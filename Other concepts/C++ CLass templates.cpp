832eb939de1b8b8b615d9b5ca53194803bc66143
Vedant Kumar <124188416+Xvedk@users.noreply.github.com>
2023-10-26 00:25:23 +0530

    Create C++ CLass templates.cpp

---
+++ Other concepts/C++ CLass templates.cpp
@@ -0,0 +1,18 @@
+template<typename T>
+class AddElements
+{
+    T t_;
+public:
+    AddElements(const T& t) : t_(t)
+    {}
+
+    T add(const T& r) const
+    {
+        return t_ + r;
+    }
+
+    T concatenate(const T& r) const
+    {
+        return t_ + r;
+    }
+};
